f4b0e9177dbbef3927a353a6026086831eb8fbe8
Fumiro Yoshihara <fumiro1989@gmail.com>
2018-12-05 20:37:23 +0900

    書籍情報新規登録と削除機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,41 @@
 class BooksController < ApplicationController
-  before_action :set_book, only: [:show]
+  before_action :set_book, only: [:show, :destroy]
 
   def index
-    @books = Book.all
+    @books = Book.all.order(id: 'desc')
   end
 
   def new
     @book = Book.new
   end
 
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "書籍を登録しました"
+      redirect_to(books_path)
+    else
+      render(new_book_path)
+    end
+  end
+
   def show
+  end
+
+  def destroy
+    @book.destroy
+    respond_to do |format|
+      format.html { redirect_to books_url, notice: '書籍情報を削除しました。' }
+      format.json { head :no_content }
+    end
   end
 
   private
     def set_book
       @book = Book.find(params[:id])
+    end
+
+    def book_params
+      params.require(:book).permit(:title, :price, :pages, :bought_at, :purpose)
     end
 end
