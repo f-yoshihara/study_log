b945d3d455c5efcb3ce0b776cc86c17a99d5341c
Fumiro Yoshihara <fumiro1989@gmail.com>
2018-12-07 19:44:11 +0900

    $ rails g controller contents index show edit new

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'contents/index'
+  get 'contents/show'
+  get 'contents/edit'
+  get 'contents/new'
   resources :snippets
   resources :books
   get '/' => 'kanban#index'
