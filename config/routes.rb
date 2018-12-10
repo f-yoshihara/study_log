5b5a3796c5aef3fd65603f9537c41fc14a02b930
Fumiro Yoshihara <fumiro1989@gmail.com>
2018-12-11 02:54:42 +0900

    fix parts index's routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,20 @@
 Rails.application.routes.draw do
+  resources :post_its, except: :show
   namespace :post_its do
+    get  'kanban'    => 'kanban#index'
     post 'parts/:id' => 'parts#create' #post_its_parts_path
-    get 'parts/:id' => 'parts#new'
-    get 'parts/:id/index' => 'parts#index'
-    get 'kanban/' => 'kanban#index'
+    get  'parts/:id' => 'parts#new'
+    get  '/:id'      => 'parts#index'
   end
+  resources :books
   namespace :books do
-    get 'kanban/' => 'kanban#index'
+    get 'kanban' => 'kanban#index'
   end
-  resources :post_its
-  get  'auth/'        => 'auth#new'
+  get  'auth'         => 'auth#new'
   post 'auth/create'  => 'auth#create'
   get  'auth/destroy' => 'auth#destroy'
   resources :users
-  get 'contents/:id/new' => 'contents#new'
   resources :contents, except: [:new]
+  get 'contents/:id/new' => 'contents#new'
   resources :snippets
-  resources :books
 end
