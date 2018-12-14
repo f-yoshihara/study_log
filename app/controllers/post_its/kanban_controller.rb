e34971f8a6dd90c76aed688b3f7e296bed065be2
Fumiro Yoshihara <fumiro1989@gmail.com>
2018-12-14 19:32:30 +0900

    ゴミ箱に二週間以上入っているpost-itが削除される機能追加

--- app/controllers/post_its/kanban_controller.rb
+++ app/controllers/post_its/kanban_controller.rb
@@ -1,8 +1,12 @@
 class PostIts::KanbanController < ApplicationController
-  before_action :set_backlog,     only: [:index, :backlog]
-  before_action :set_in_progress, only: [:index, :in_progress]
-  before_action :set_done,        only: [:index, :done]
-  before_action :set_deleted,     only: :deleted
+  before_action :set_backlog,       only: [:index, :backlog]
+  before_action :set_in_progress,   only: [:index, :in_progress]
+  before_action :set_done,          only: [:index, :done]
+  before_action :destroy_old_trash, only: :deleted
+
+  def deleted
+    set_deleted
+  end
 
   private
     def set_backlog
@@ -19,5 +23,9 @@ class PostIts::KanbanController < ApplicationController
 
     def set_deleted
       @deleted = PostIt.where(status: :deleted)
+    end
+
+    def destroy_old_trash
+      PostIt.where('status = ? and deleted_at<=?', 3, Time.current.ago(14.days)).delete_all
     end
 end
