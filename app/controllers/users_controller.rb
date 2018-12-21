051304860238d7c08020db10290f8978dc641103
Fumiro Yoshihara <fumiro1989@gmail.com>
2018-12-21 22:31:21 +0900

    user 登録追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,8 +15,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = "新規登録が完了しました"
-      redirect_to(users_path)
+      reset_session
+      session[:user] = @user.id
+      flash[:notice] = "新規登録が完了しました。"
+      redirect_to '/'
     else
       render(new_user_path)
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
   has_many :snippets, dependent: :destroy
+  validates :name,
+    presence: true
+  validates :password_digest,
+    presence: true
+  validates :email,
+    presence: true,
+    uniqueness: true,
+    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
